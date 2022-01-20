f09f5889e49d07e8de87590250e1bf164ccf3d77
Kenny Lee <kenny.lee927@gmail.com>
2022-01-20 15:21:25 -0500

    added button to receive user input

--- scripts/main.js
+++ scripts/main.js
@@ -5,19 +5,26 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
+    this.info = function () {
         return this.title + " by " + this.author + ", " + this.pages + " pages, " + read;
     }
     // the constructor...
 }
 
 function addBookToLibrary() {
-const newBook = new Book ("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
-myLibrary.push(newBook);
+    const newBook = new Book(prompt("Enter book title..", "The Hobbit"), prompt("Enter book author..", "J.R.R. Tolkien"), prompt("Enter number of pages..", "295"), prompt("Read:Y/N", "Y"));
+    myLibrary.push(newBook);
+}
+
+function display() {
+    console.log(myLibrary);
 }
 
 
+const newB = document.getElementById("newButton");
+newB.addEventListener("click", function () {
+    addBookToLibrary();
+    display();
+});
 
-
-addBookToLibrary();
 console.log(myLibrary);
